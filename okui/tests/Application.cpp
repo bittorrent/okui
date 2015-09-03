ee4ace8185502fbad317cc1036e6a2c6b1c6335b
Christopher Brown <cbrown@bittorrent.com>
2015-09-02 17:24:30 -0700

    just run a server for the application unit test rather than use random.org

--- okui/tests/Application.cpp
+++ okui/tests/Application.cpp
@@ -2,37 +2,62 @@
 
 #include "onair/okui/config.h"
 
+#include "onair/HTTPConnection.h"
+#include "onair/TCPAcceptor.h"
+
 #include "TestApplication.h"
 
 using namespace onair;
 using namespace onair::okui;
 
+namespace {
+    std::string gTestServerResponse;
+}
+
+struct TestHTTPConnection : HTTPConnection {
+    using HTTPConnection::HTTPConnection;
+    virtual void handleRequest(const Request& request) override {
+        sendResponse("HTTP/1.1 200 OK", gTestServerResponse.data(), gTestServerResponse.size());
+    }
+};
+
+struct TestServer : TCPAcceptor<TestHTTPConnection> {    
+    std::string response;
+};
+
 TEST(Application, download) {
     TestApplication application;
-    auto result = application.download("https://www.random.org/integers/?num=1&min=12340&max=12341&col=1&base=10&format=plain").get();
+    TestServer server;
+    server.start(Address(), 12654);
+
+    gTestServerResponse = "expected";
+    auto result = application.download("http://localhost:12654/").get();
     
     ASSERT_TRUE(result != nullptr);
-    EXPECT_TRUE(*result == "12340\n" || *result == "12341\n");
+    EXPECT_EQ(*result, "expected");
 }
 
 TEST(Application, downloadCaching) {
     TestApplication application;
+    TestServer server;
+    server.start(Address(), 12654);
 
-    auto dl1 = application.download("https://www.random.org/integers/?num=1&min=-1000000000&max=1000000000&col=1&base=10&format=plain");
-    auto dl2 = application.download("https://www.random.org/integers/?num=1&min=-1000000000&max=1000000000&col=1&base=10&format=plain");
-    
+    gTestServerResponse = "response 1";
+    auto dl1 = application.download("http://localhost:12654/");    
     auto result1 = dl1.get();
     ASSERT_TRUE(result1 != nullptr);
 
+    gTestServerResponse = "response 2";
+    auto dl2 = application.download("http://localhost:12654/");
     auto result2 = dl2.get();
     ASSERT_TRUE(result2 != nullptr);
 
     EXPECT_EQ(*result1, *result2);
 
-    auto dl3 = application.download("https://www.random.org/integers/?num=1&min=-1000000000&max=1000000000&col=1&base=10&format=plain", false);
+    gTestServerResponse = "response 3";
+    auto dl3 = application.download("http://localhost:12654/", false);
     auto result3 = dl3.get();
     ASSERT_TRUE(result3 != nullptr);
 
-    // there's a 1 in 2000000001 chance to get a false negative here :-/
     EXPECT_NE(*result2, *result3);
 }
