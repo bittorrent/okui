d76694cd6afa2c861f07f243606e533169d3e094
Mike Naquin <mike.naquin@bittorrent.com>
2015-11-06 13:37:34 -0800

    okui Xcode tests
    [This commit was only partially migrated from livelib commit 0a2099015e0b3edf30358abafc8ff10b661deecb.]

---
+++ okui/okui-tests-ios-host/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  okui-tests-ios-host
+//
+//  Created by Work on 11/6/15.
+//  Copyright © 2015 BitTorrent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ okui/okui-tests-tvos-host/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  okui-tests-tvos-host
+//
+//  Created by Work on 11/6/15.
+//  Copyright © 2015 BitTorrent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
